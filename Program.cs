ee0d188edea0ed01688eb3bd35ba14eea767fd14
Breno Gomes <breno.jgomes2@gmail.com>
2022-11-11 01:26:11 -0300

    Finalizando desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Nokia nokia = new Nokia("99999", "Tijolão", "123", 50);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone IPhone:");
+Iphone iphone = new Iphone("123456", "Iphone-13", "123", 1000);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
